c09d9d7ff04be9622972fb5221142fff5e738efd
Abinash Karki <abinashsparadise@Abinashs-MacBook-Air.local>
2023-09-14 21:07:51 +0545

    This is added in the github

---
+++ JS/searchingMeal.js
@@ -0,0 +1,13 @@
+const inputNameMeal =document.querySelector('.inputMealName')
+const searchbtn=document.querySelector('.btn')
+
+searchbtn.addEventListener('click' , result);
+
+function result (){
+
+const mealName=inputNameMeal.value
+console.log(mealName);
+localStorage.setItem('storeValue', mealName);
+
+  
+}
